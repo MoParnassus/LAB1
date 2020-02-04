52b2c5ba07207e94b9d439eb873700f552b4c0b2
MoParnassus <56222484+MoParnassus@users.noreply.github.com>
2020-02-04 07:41:07 +0100

    Signed-off-by: MoParnassus <56222484+MoParnassus@users.noreply.github.com>

---
+++ Lab_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3
+{
+    class Hello
+    {
+        public static void WriteHello()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
